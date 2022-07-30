b315894bdb90d7db1ca9e64651184db39be2e76b
Macro <fuhong.zhou@qq.com>
2022-07-31 06:26:02 +0800

    .

--- Models.cs
+++ Models.cs
@@ -7,26 +7,20 @@ using DKCommunicationNET. Interface;
 
 namespace DKCommunicationNET;
 
+/// <summary>
+/// 设备型号列表，关联协议类型
+/// </summary>
+
 public enum Models
 {
+    Hex81 = 81,
+    Hex5AA5 = 510,
     DK34B1 = 81,
     DK34B2 = 81,
     DK34F1 = 81,
     DK34B3 = 510,
     DKPTS1 = 510,
-    DKPTS = 510,
-    Hex81 = 81,
-    Hex5AA5 = 510,
+    DKPTS = 510,   
 }
 
-//public enum Models
-//{
-//    DK34B1 = 81,
-//    DK34B2 = 81,
-//    DK34F1 = 81,
-//    DK34B3 = 510,
-//    DKPTS1 = 510,
-//    DKPTS = 510,
-//    Hex81 = 81,
-//    Hex5AA5=510
-//}
+
