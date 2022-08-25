cc8f052ce9435aba56ad0a3b05c568e161a366c6
Macro <fuhong.zhou@qq.com>
2022-08-26 02:01:18 +0800

    .

--- Protocols/Hex81/Hex81PacketBuilder_EPQ.cs
+++ Protocols/Hex81/Hex81PacketBuilder_EPQ.cs
@@ -5,33 +5,43 @@ using System. Text;
 using System. Threading. Tasks;
 using DKCommunicationNET. Core;
 
-namespace DKCommunicationNET. Protocols. Hex81
+namespace DKCommunicationNET. Protocols. Hex81;
+
+/// <summary>
+/// 电能模块报文创建类
+/// </summary>
+internal class Hex81PacketBuilder_EPQ : IPacketBuilder_PQ
 {
-    internal class Hex81PacketBuilder_EPQ : IPacketBuilder_PQ
+    /// <summary>
+    /// 设备ID
+    /// </summary>
+    readonly ushort _id;
+
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    /// <param name="id">设备ID</param>
+    public Hex81PacketBuilder_EPQ ( ushort id )
     {
-       readonly ushort _id;
-        public Hex81PacketBuilder_EPQ ( ushort id )
-        {
-            _id = id;
-        }
+        _id = id;
+    }
 
-        public OperateResult<byte[ ]> Packet_ReadData ( )
-        {
-            return Hex81PacketBuilderHelper. Instance. PacketShellBuilder ( Hex81Information. ReadData_EPQ , _id );
-        }
+    public OperateResult<byte[ ]> Packet_ReadData ( )
+    {
+        return Hex81PacketBuilderHelper. Instance. PacketShellBuilder ( Hex81Information. ReadData_EPQ , _id );
+    }
 
-        public OperateResult<byte[ ]> Packet_SetElectricity ( byte electricityType , float meterPConst , float meterQConst , float sourcePConst , float sourceQConst , uint meterDIV , uint meterRounds ,IByteTransform byteTransform)
-        {
-            byte[ ] data=new byte[ 25 ];
-            data[ 0 ] = electricityType;
-            byteTransform. TransByte ( meterPConst ). CopyTo ( data , 1);
-            byteTransform. TransByte ( meterQConst ). CopyTo ( data , 5 );
-            byteTransform. TransByte ( sourcePConst ). CopyTo ( data , 9 );
-            byteTransform. TransByte ( sourceQConst ). CopyTo ( data , 13 );
-            BitConverter.GetBytes ( meterDIV ). CopyTo ( data , 17 );
-            BitConverter.GetBytes ( meterRounds ). CopyTo ( data , 21 );
+    public OperateResult<byte[ ]> Packet_SetElectricity ( byte electricityType , float meterPConst , float meterQConst , float sourcePConst , float sourceQConst , uint meterDIV , uint meterRounds , IByteTransform byteTransform )
+    {
+        byte[ ] data = new byte[25];
+        data[0] = electricityType;
+        byteTransform. TransByte ( meterPConst ). CopyTo ( data , 1 );
+        byteTransform. TransByte ( meterQConst ). CopyTo ( data , 5 );
+        byteTransform. TransByte ( sourcePConst ). CopyTo ( data , 9 );
+        byteTransform. TransByte ( sourceQConst ). CopyTo ( data , 13 );
+        BitConverter. GetBytes ( meterDIV ). CopyTo ( data , 17 );
+        BitConverter. GetBytes ( meterRounds ). CopyTo ( data , 21 );
 
-            return Hex81PacketBuilderHelper. Instance. PacketShellBuilder ( Hex81Information. SetElectricity , Hex81Information. SetElectricity_Length , _id );
-        }
+        return Hex81PacketBuilderHelper. Instance. PacketShellBuilder ( Hex81Information. SetElectricity , Hex81Information. SetElectricity_Length , _id );
     }
 }
